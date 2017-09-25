4a8e7e27e48609af9cbeae343d807271087cc130
dpacheco <david.pacheco@beubi.com>
2017-09-25 18:43:53 +0100

    BEEWEB-207 PT translations

--- compile-translations.sh
+++ compile-translations.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Portuguese language
+msgfmt translations/pt/LC_MESSAGES/messages.po --output-file translations/pt/LC_MESSAGES/messages.mo
+msgfmt src/octoprint/translations/pt/LC_MESSAGES/messages.po --output-file src/octoprint/translations/pt/LC_MESSAGES/messages.mo
+
 # German language
 msgfmt translations/de/LC_MESSAGES/messages.po --output-file translations/de/LC_MESSAGES/messages.mo
 msgfmt src/octoprint/translations/de/LC_MESSAGES/messages.po --output-file src/octoprint/translations/de/LC_MESSAGES/messages.mo
