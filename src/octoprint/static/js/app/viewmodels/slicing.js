c906248cac361185fd369de1c866bcd194cd4e31
dpacheco <david.pacheco@beubi.com>
2018-01-29 11:43:14 +0000

    BEEWEB-240 remove unnecessary code

--- src/octoprint/static/js/app/viewmodels/slicing.js
+++ src/octoprint/static/js/app/viewmodels/slicing.js
@@ -4,7 +4,6 @@ $(function() {
 
         self.loginState = parameters[0];
         self.printerProfiles = parameters[1];
-        //self.printerState = parameters[2];
 
         self.file = ko.observable(undefined);
         self.target = undefined;
@@ -203,14 +202,11 @@ $(function() {
                 && self.slicer() != undefined
                 && self.sliceButtonControl()
                 && !self.estimationDialog();
-                //&& self.profile() != undefined
-                //&&( !(self.printerState.isPrinting() || self.printerState.isPaused()) || !self.slicerSameDevice());
         });
 
         self.sliceButtonTooltip = ko.pureComputed(function() {
             if (!self.enableSliceButton()) {
-                if (//(self.printerState.isPrinting() || self.printerState.isPaused()) &&
-                    self.slicerSameDevice()) {
+                if (self.slicerSameDevice()) {
                     return gettext("Cannot slice on the same device while printing");
                 } else {
                     return gettext("Cannot slice, not all parameters specified");
@@ -707,7 +703,7 @@ $(function() {
 
     OCTOPRINT_VIEWMODELS.push([
         SlicingViewModel,
-        ["loginStateViewModel", "printerProfilesViewModel", /*"printerStateViewModel"*/],
+        ["loginStateViewModel", "printerProfilesViewModel"],
         "#slicing_configuration_dialog"
     ]);
 });
