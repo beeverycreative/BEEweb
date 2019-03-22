a1d5830700b665253744362c3040d26bad807d89
Sílvia M.G. Rodrigues <silviamgr.sissi@gmail.com>
2019-03-22 14:15:52 +0000

    Change values of config vars accordingly to the new firmware BEEVC-BEETHEFIRST-10.5.50

--- src/octoprint/util/BEEt_config.py
+++ src/octoprint/util/BEEt_config.py
@@ -19,15 +19,14 @@ config['steps_per_mm_e0']=441.390           ## p/ a beethefirst
 #config['steps_per_mm_y']=...
 #config['steps_per_mm_z']=...
 ACCELERATION_TICKS_PER_SECOND=1000.0
-JUNCTION_DEVIATION=0.05         ## /?\
-MINIMUM_PLANNER_SPEED=0.0
+JUNCTION_DEVIATION=0.5                      ## -> valor para o firmware BEEVC-BEETHEFIRST-10.5.50
+MINIMUM_PLANNER_SPEED=0.5                   ## -> também p/ o 10.5.50.
 MAX_FEED_X=60000.0
 MAX_FEED_Y=60000.0
 MAX_FEED_Z=60000.0
 MAX_FEED_E0=60000.0
 MAX_SIZE_BUFFER = 3                         ## /!\ atenção
                                             ## -> esta variável vai determinar o tamanho do buffer, para imitar o firmware
-
 
 ## definição de variáveis globais:
 global verbose
